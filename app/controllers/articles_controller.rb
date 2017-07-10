73fe4cd86385433bcc88fd1acdca761bb7dcc701
manelgavalda <expedientmanel@gmail.com>
2017-07-10 21:22:03 +0200

    Added parameters validation on form from controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,8 +12,8 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: params[:article][:title],
-                          body: params[:article][:body])
+    @article = Article.new(article_params)
+
     if @article.save
       redirect_to @article
     else
@@ -25,5 +25,11 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title,:body)
   end
 end
