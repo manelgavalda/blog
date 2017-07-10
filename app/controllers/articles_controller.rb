cef7d8dc52c77e4625ab369fe6eb696b3c138d71
manelgavalda <expedientmanel@gmail.com>
2017-07-10 22:16:26 +0200

    Added devise gem for authentication with basic use and configuration, layout for forms and methods to update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,13 +11,26 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
-    @article = Article.new(article_params)
+    @article = Article.new(params[:title])
 
     if @article.save
       redirect_to @article
     else
       render :new
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
     end
   end
 
