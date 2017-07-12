35f77b45615817f06fe56f2c50cd812f5c1f6861
manelgavalda <expedientmanel@gmail.com>
2017-07-13 00:15:10 +0200

    Added callbacks before methods on controller and model to refractor also added views_count on each article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_article, except: [:index, :new, :create]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article.update_visits_count
   end
 
   def new
@@ -12,7 +14,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -25,7 +26,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -34,12 +34,15 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title,:body)
