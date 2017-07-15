3ab15cd7ca9a0ad0fdb6de708c068b9a47c88630
manelgavalda <expedientmanel@gmail.com>
2017-07-15 15:18:09 +0200

    Added nested comments for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
+  resources :articles do
+    resources :comments, only: [:create, :destroy, :update]
+  end
   devise_for :users
   root 'welcome#index'
-
-  resources :articles
 end
