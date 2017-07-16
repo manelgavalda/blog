3cd1c1626054ee8175cb44dfe9ac2b31e7d467c0
manelgavalda <expedientmanel@gmail.com>
2017-07-16 22:20:42 +0200

    Added many to many relationship with categories and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170716193252) do
+ActiveRecord::Schema.define(version: 20170716195040) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 20170716193252) do
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "has_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_has_categories_on_article_id"
+    t.index ["category_id"], name: "index_has_categories_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
